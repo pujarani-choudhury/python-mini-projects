bec580494c3e78987c8445fab1808f6e16378ee4
pujarani-choudhury <pujac3010@gmail.com>
2025-11-06 14:11:46 +0530

    Added day 3 Rock Paper Scisscrs game

---
+++ basics/rock_paper_scissor/README.md
@@ -0,0 +1,13 @@
+# ✊ Rock-Paper-Scissors (CLI Game)
+
+A simple Python command-line game where you play Rock-Paper-Scissors against the computer.
+
+## 🚀 Features
+- Random computer choices
+- Simple win/lose logic
+- Option to play again without restarting the program
+- Beginner-friendly and easy to understand
+
+## 🕹️ How to Run
+```bash
+python day3_rock_paper_scissor.py
---
+++ basics/rock_paper_scissor/day3_rock_paper_scissor.py
@@ -0,0 +1,33 @@
+import random
+
+def play():
+    user = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors:")
+    computer = random.choice(['r', 'p', 's'])
+    print(f"Computer chose: {computer}")
+
+    if user == computer:
+        return "Its a tie!"
+
+    # r>s, s>p, p>r
+    if is_win(user, computer):
+        return 'You won!'
+    return 'You lost!'
+    
+def is_win(player, opponent):
+    # return True if player wins
+    if (player == 'r' and opponent == 's') or \
+       (player == 's' and opponent == 'p') or \
+       (player == 'p' and opponent == 'r'):
+        return True
+print(play())
+
+
+# --- Main Game Loop ---
+while True:
+    print("\n Rock Paper Scissors Game ")
+    print(play())
+
+    again = input("Do you want to play again? (y/n): ").lower()
+    if again != 'y':
+        print("Thanks for playing! Goodbye!")
+        break
